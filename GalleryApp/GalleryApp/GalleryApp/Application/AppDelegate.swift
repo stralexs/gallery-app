c12344a7fdddf8cdb7d51df6952f8b0df8eb6a7a
Alexander <alexandersivko.swift@gmail.com>
2024-06-15 15:50:55 +0300

    appdelegate codestyle

--- GalleryApp/GalleryApp/GalleryApp/Application/AppDelegate.swift
+++ GalleryApp/GalleryApp/GalleryApp/Application/AppDelegate.swift
@@ -11,11 +11,18 @@ import UIKit
 @main class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: Methods
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
